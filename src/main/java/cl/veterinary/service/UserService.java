1d84a3900b3275bd09babe7b744a135389f65318
matfuentesv <mat.fuentes@duocuc.cl>
2025-04-09 22:16:46 -0400

    se eliminN metodos

--- src/main/java/cl/veterinary/service/UserService.java
+++ src/main/java/cl/veterinary/service/UserService.java
@@ -1,13 +1,12 @@
 package cl.veterinary.service;
 
-import cl.veterinary.model.User;
-
-import java.util.List;
 import java.util.Optional;
+
+import cl.veterinary.model.User;
 
 public interface UserService {
 
-    List<User> findAll();
+   
     Optional<User> findUserById(Long id);
     User saveUser(User user);
     User updateUser(User user);
--- src/main/java/cl/veterinary/service/impl/UserServiceImpl.java
+++ src/main/java/cl/veterinary/service/impl/UserServiceImpl.java
@@ -1,13 +1,13 @@
 package cl.veterinary.service.impl;
 
-import cl.veterinary.model.User;
-import cl.veterinary.repository.UserRepository;
-import cl.veterinary.service.UserService;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import cl.veterinary.model.User;
+import cl.veterinary.repository.UserRepository;
+import cl.veterinary.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,11 +16,6 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
-
-    @Override
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
 
     @Override
     public Optional<User> findUserById(Long id) {
